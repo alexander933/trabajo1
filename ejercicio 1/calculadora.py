4323c97b0c0728bd9f3527a8bc51272f2d1d4db2
alexander933 <93534178+alexander933@users.noreply.github.com>
2024-01-20 15:42:18 -0500

    ejercicio1 completo

--- ejercicio 1/calculadora.py
+++ ejercicio 1/calculadora.py
@@ -1,19 +1,19 @@
-def calculadora():
-    num1 = float("Ingrese el primer número: ")
-    num2 = float("Ingrese el segundo número: ")
+def calculadora(): #define la funcion, tipo de dato y variable a ingresar.
+    num1 = float(input("Ingrese el primer número: ")) #faltaba el input
+    num2 = float(input("Ingrese el segundo número: ")) #faltaba el input
     operacion = input("Ingrese la operación (+, -, *, /): ")
 
     if operacion == '+':
-        resultado = num + num2
+        resultado = num1 + num2 #variable incorrecta
     elif operacion == '-':
         resultado = num1 - num2
     elif operacion == '*':
         resultado = num1 * num2
     elif operacion == '/':
-        resultado = num / num2
+        resultado = num1 / num2 #variable incorrecta
     else:
         resultado = "Operación no válida"
 
-    print("Resultado: "resultado)
+    print ("Resultado: ", resultado) #faltaba una coma
 
-calculdora()
+calculadora() #error de escritura
