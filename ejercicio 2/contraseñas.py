8980474e182f0607422dfeccba8daf576b896fbb
alexander933 <93534178+alexander933@users.noreply.github.com>
2024-01-27 11:12:09 -0500

    Update contraseñas.py
    terminado

--- ejercicio 2/contraseñas.py
+++ ejercicio 2/contraseñas.py
@@ -1,9 +1,9 @@
-import random #error de escritura
+import random #error de escritura #importa el modulo random
 import string #importa los tipos de variables
 
-def generar_contraseña(longitud=8): #hacia falta definir la variable con def
-    caracteres = string.ascii_letters + string.digits + string.punctuation
-    contraseña = ''.join(random.choice(caracteres) for _ in range(longitud))
+def generar_contraseña(longitud=8): #hacia falta definir la variable con def #define la funcion
+    caracteres = string.ascii_letters + string.digits + string.punctuation #crea la variable y le da un valor
+    contraseña = ''.join(random.choice(caracteres) for _ in range(longitud)) #crea la variable y le da un valor
     return contraseña #retornar contraseña
 
-print("contraseña_generada: ",generar_contraseña()) #error de escritura
+print("contraseña_generada: ",generar_contraseña()) #error de escritura. #imprime la contraseña aleatoria
