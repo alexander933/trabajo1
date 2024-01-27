14dd19233aa8e8f869efb7bd8795829b2c99a62d
alexander933 <93534178+alexander933@users.noreply.github.com>
2024-01-27 11:14:21 -0500

    terminado

--- ejercicio 2/contraseñas.py
+++ ejercicio 2/contraseñas.py
@@ -4,6 +4,6 @@ import string #importa los tipos de variables
 def generar_contraseña(longitud=8): #hacia falta definir la variable con def #define la funcion
     caracteres = string.ascii_letters + string.digits + string.punctuation #crea la variable y le da un valor
     contraseña = ''.join(random.choice(caracteres) for _ in range(longitud)) #crea la variable y le da un valor
-    return contraseña #retornar contraseña
+    return contraseña #retornar contraseña.
 
 print("contraseña_generada: ",generar_contraseña()) #error de escritura. #imprime la contraseña aleatoria
