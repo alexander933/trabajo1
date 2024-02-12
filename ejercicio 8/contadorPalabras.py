64eccd512f49f90b83b8ff56ba810c0af630421a
alexander933 <93534178+alexander933@users.noreply.github.com>
2024-02-12 09:17:51 -0500

    terminado

--- ejercicio 8/contadorPalabras.py
+++ ejercicio 8/contadorPalabras.py
@@ -1,11 +1,15 @@
-def contar_palabras_en_archivo(nombre_archivo)
+# Definición de la función para contar palabras en un archivo
+def contar_palabras_en_archivo(archivo_nombre): #faltaba definir la funcion (def)
     try:
-        with open(nombre_archivo, 'r') as archivo:
+        with open(archivo_nombre, 'r') as archivo:
             contenido = archivo.read()
             palabras = contenido.split()
-            return len(palabras)
+            return len(palabras) #error de sintaxis
     except FileNotFoundError:
-        retunr f"El archivo {nombre_archivo} no fue encontrado."
+        return f"El archivo {archivo_nombre} no fue encontrado." #error de sintaxis
 
-archivo_nombre = input("Ingrese el nombre del archivo de texto: ")
-print(f"El archivo contiene {contar_palabras_en_archivo(archivo_nombre)} palabras.")
+# Solicitar al usuario que ingrese el nombre del archivo de texto
+archivo_nombre = input("Ingrese el nombre del archivo de texto: ") #la variable estaba invertida
+
+# Imprimir el número de palabras en el archivo
+print(f"El archivo contiene {contar_palabras_en_archivo(archivo_nombre)} palabras.") #la variable estaba invertida
--- ejercicio 9/dados.py
+++ ejercicio 9/dados.py
@@ -1,10 +1,10 @@
-import radom
+import random #error de sintaxtis #importa el modulo random
 
-simular_lanzamiento_dados(cantidad_dados, caras_por_dado)
-    resultados = [random.randint(1, caras_por_dado) for _ in range(cantidad_dados)]
-    retunr resultados
+def simular_lanzamiento_dados(cantidad_dados, caras_por_dado): #faltaba definir la funcion(def) y dos puntos #define la funcion y toma dos argumentos
+    resultados = [random.randint(1, caras_por_dado) for _ in range(cantidad_dados)] #se genera una lista llamada resultados, que contiene los resultados de la tirada de los dados
+    return resultados #error de sintaxis #la funcion devuelve el resultado de la tirada de los dados
 
-cantidad_dados = int(input("Ingrese la cantidad de dados a lanzar: "))
-caras_por_dado = int(input("Ingrese la cantidad de caras por dado: "))
-lanzamientos = simular_lanzamiento_dados(, caras_por_dado)
-print(f"Resultados del lanzamiento: {lanzamientos}")
+cantidad_dados = int(input("Ingrese la cantidad de dados a lanzar: ")) #solicita ingresar la cantidad de dados y convierte la  cifra a entero
+caras_por_dado = int(input("Ingrese la cantidad de caras por dado: ")) #solicita ingresar la cantidad de caras por dado y convierte la cifra a entero
+lanzamientos = simular_lanzamiento_dados(cantidad_dados, caras_por_dado) #faltaba un argumento #se llama la funcion con los valores ingresados, el resultado se guarda en lanzammientos
+print(f"Resultados del lanzamiento: {lanzamientos}") #se imprime en pantalla en resultado del lanzamiento de los dados, utilizando una cadena formateada donde lanzamientos se sustituye por el resultado de la funcion
